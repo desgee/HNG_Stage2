f1f0d9d2bf342d5df67c1f218c84e2431f004351
desgee <152696726+desgee@users.noreply.github.com>
2024-07-08 11:14:12 +0100

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager
+
+
+#initialize flask app
+app = Flask(__name__)
+ # Configuration
+app.config['SECRET_KEY'] = 'your-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres.sucilzhqzugjcucykwmo:rT12dIOsWoqV2wln@aws-0-eu-central-1.pooler.supabase.com:6543/postgres'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+
+
+
+#instance of the db
+db = SQLAlchemy(app)
+jwt = JWTManager(app) 
+
+
+
+
+
+
+
+from app import routes
---
+++ app/routes.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
